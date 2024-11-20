f013c918f8f4401ebaca4446822e04f1aa3ba7c3
David Akinola <dakinola54@gmail.com>
2024-11-20 12:09:32 +0100

    double last hand

--- Cards/cards.py
+++ Cards/cards.py
@@ -97,4 +97,10 @@ def maybe_double_last(hand):
     :return: list - hand with Jacks (if present) value doubled.
     """
 
-    pass
+    if hand[-1] == 11:
+        hand[-1] = 11 * 2
+        return hand
+    else:
+        return hand
+
+print(maybe_double_last([5, 9, 10]))
